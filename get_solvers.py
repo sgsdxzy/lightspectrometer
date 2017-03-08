97320d386b2d4e3cee49c008002ae8060a84f42b
Light <sgsdxzy@gmail.com>
2017-03-08 12:38:17 +0800

    speed comparison

--- get_solvers.py
+++ get_solvers.py
@@ -4,6 +4,7 @@ import numpy as np
 #B_data = np.loadtxt("/home/sgsdxzy/Sync/Programs/light-spectrometer/magnet2.txt", skiprows=4)
 spec = sp.pySpectrometer.fromfile("/home/sgsdxzy/Sync/Programs/light-spectrometer/spectrometer1.npz")
 #spec.init(B_data, x_delta=0.002, y_delta=0.0025, x_offset=140e-3, y_offset=-110e-3, dt_multiplier=0.0001, maxtime=1e-4)
+spec.settimemultiplyer(0.01)
 side, front = spec.getSolvers(np.arange(30, 400, 0.1, dtype=float), np.arange(-10,11,1, dtype=float))
 if side is not None :
     side.save("/home/sgsdxzy/Sync/Programs/light-spectrometer/side_solver.npz")
